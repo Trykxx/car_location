32d3911ccc74693cddfdca46b5557e7e73fe821d
Trykxx <amitnicolas@gmail.com>
2024-07-08 09:57:16 +0200

    first commit

---
+++ src/Entity/Test.php
@@ -0,0 +1,19 @@
+<?php
+// namespace
+
+// Creer une classe Test
+    // methode publique display
+    // retourne hello world !
+
+namespace App\Entity;
+
+class Test
+{
+    /**
+     * @return string
+     */
+    public function display():string
+    {
+        return "Hello world !";
+    }
+}
